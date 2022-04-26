59686aa745216c94360044840d192b32b4414cba
Patrick Pluequett <ppluequett@gmail.com>
2022-04-26 16:26:01 +0200

    add request to create new or update existing user

--- src/controller/user.js
+++ src/controller/user.js
@@ -4,14 +4,18 @@ const createUser = (req, res) => {
   const id = req.params.id;
   const { name, email } = req.body;
 
-  User.create({
-    id: id,
-    name: name,
-    email: email,
-    // favorites: ['235423sdfsd', '2332dassdfsa', '243dfs'],
-    // registeredTrees: ['r235423sdfsd', 'r2332dassdfsa', 'r243dfs'],
-    // picture: 'www.picture.de/2222',
-  }).then((tree) => res.send(tree));
+  User.findOneAndUpdate(
+    { id: id },
+    {
+      id: id,
+      name: name,
+      email: email,
+      // favorites: ['235423sdfsd', '2332dassdfsa', '243dfs'],
+      // registeredTrees: ['r235423sdfsd', 'r2332dassdfsa', 'r243dfs'],
+      // picture: 'www.picture.de/2222',
+    },
+    { upsert: true, new: true }
+  ).then((tree) => res.send(tree));
 };
 
 const getAllUsers = (req, res) => {
