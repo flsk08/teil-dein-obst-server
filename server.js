c593b4c9ff39fa8683c3ac68c32d3dd60f790e4a
Meike <meikehuettl@web.de>
2022-04-26 17:35:35 +0200

    mock data for map

--- server.js
+++ server.js
@@ -1,14 +1,32 @@
 const express = require('express');
 require("dotenv").config();
 const mongoose = require("mongoose");
+const fs = require('fs')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+
+
 
 const app = express();
+app.use(cors())
+app.use(bodyParser.json())
+app.use(express.json())
 
-const port = 3000;
+
+
+const port = 8000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+app.get('/coordinates', (req, res) => {
+  fs.readFile('./location.json', 'utf8', (err, data) => {
+      res.send(JSON.parse(data));
+  })
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
