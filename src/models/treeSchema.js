2a65f8cf0ddb91c6db2ad910541b963c020f575e
Patrick Pluequett <ppluequett@gmail.com>
2022-04-27 14:07:29 +0200

    change treeSchema

--- src/models/treeSchema.js
+++ src/models/treeSchema.js
@@ -2,9 +2,22 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const treeSchema = new Schema({
-  type: String,
-  location: String,
-  active: Boolean,
+  id: String,
+  type: {
+    type: String,
+  },
+  coordinates: {
+    lat: String,
+    lng: String,
+  },
+  location: {
+    strasse: String,
+    plz: String,
+    stadt: String,
+  },
+  status: {
+    status: String,
+  },
   info: String,
   comments: Array,
   picture: String,
